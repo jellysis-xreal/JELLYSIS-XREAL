75689249771f59a2e4c27ffd17cd346085d5e9f7
cmins1011 <chlalstj1005@gmail.com>
2023-08-10 04:41:59 +0900

    [Multi] cut strawberry

--- Assets/Netcode Test/temp/Scripts/NetworkPlayerRpcCall.cs
+++ Assets/Netcode Test/temp/Scripts/NetworkPlayerRpcCall.cs
@@ -4,29 +4,29 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class NetworkPlayerRpcCall : NetworkBehaviour
 {
-    public void OnSelectGrabbable(SelectEnterEventArgs eventArgs)
-    {
-        Debug.Log("[TEST] Grabbed!");
-        if (IsClient && IsOwner)
-        {
-            NetworkObject networkObjectSelected = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
-            Debug.Log("[TEST] Client selected the "+ networkObjectSelected);
-            if (networkObjectSelected != null)
-                RequestGrabbableOwnershipServerRpc(OwnerClientId, networkObjectSelected);
-        }
-    }
+    // public void OnSelectGrabbable(SelectEnterEventArgs eventArgs)
+    // {
+    //     Debug.Log("[TEST] Grabbed!");
+    //     if (IsClient && IsOwner)
+    //     {
+    //         NetworkObject networkObjectSelected = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
+    //         Debug.Log("[TEST] Client selected the "+ networkObjectSelected);
+    //         if (networkObjectSelected != null)
+    //             RequestGrabbableOwnershipServerRpc(OwnerClientId, networkObjectSelected);
+    //     }
+    // }
 
-    public void OnSelectExitGrabbable(SelectExitEventArgs eventArgs)
-    {
-        Debug.Log("[TEST] Release!!");
-        if (IsClient && IsOwner)
-        {
-            NetworkObject networkObjectSelected = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
-            Debug.Log("[TEST] Client released the "+ networkObjectSelected);
-            if (networkObjectSelected != null)
-                RequestRetrunGrabbableOwnershipServerRpc(OwnerClientId, networkObjectSelected);
-        }
-    }
+    // public void OnSelectExitGrabbable(SelectExitEventArgs eventArgs)
+    // {
+    //     Debug.Log("[TEST] Release!!");
+    //     if (IsClient && IsOwner)
+    //     {
+    //         NetworkObject networkObjectSelected = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
+    //         Debug.Log("[TEST] Client released the "+ networkObjectSelected);
+    //         if (networkObjectSelected != null)
+    //             RequestRetrunGrabbableOwnershipServerRpc(OwnerClientId, networkObjectSelected);
+    //     }
+    // }
 
     [ServerRpc]
     public void RequestGrabbableOwnershipServerRpc(ulong newOwnerClientId, NetworkObjectReference networkObjectReference)
@@ -35,6 +35,7 @@ public class NetworkPlayerRpcCall : NetworkBehaviour
         if (networkObjectReference.TryGet(out NetworkObject networkObject))
         {
             networkObject.ChangeOwnership(newOwnerClientId);
+            Debug.Log("[TEST] Now "+ networkObject + " ownerClientID: " + networkObject.OwnerClientId);
         }
     }
 
@@ -45,6 +46,16 @@ public class NetworkPlayerRpcCall : NetworkBehaviour
         if (networkObjectReference.TryGet(out NetworkObject networkObject))
         {
             networkObject.RemoveOwnership();
+            Debug.Log("[TEST] Now "+ networkObject + " ownerClientID: " + networkObject.OwnerClientId);
         }
     }
+
+    // [ServerRpc]
+    // public void RequestSetActiveServerRpc(ulong newOwnerClientId, NetworkObjectReference gameObjectReference, bool isActive)
+    // {
+    //     Debug.Log("[TEST] Got client setActive requests id: " + newOwnerClientId);
+
+    //     GameObject(gameObjectReference);//.SetActive(isActive);
+    //     Debug.Log("[TEST] SetActive " + isActive);
+    // }
 }
