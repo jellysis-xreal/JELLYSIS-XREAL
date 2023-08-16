1b71932e3a54ea431e83ab77d5c87e965723aa9d
evyfjae <46910279+evyfjae@users.noreply.github.com>
2023-08-16 16:20:41 +0900

    fix texture brushing

--- Assets/Scripts/AllBearTextureCode.cs
+++ Assets/Scripts/AllBearTextureCode.cs
@@ -45,16 +45,16 @@ public class AllBearTextureCode : MonoBehaviour
 
     public void all_brush_change_L(Texture2D brush_texture, float brush_size)
     {
-        ears_texturePaint_group[0].brushSize = brush_size * 3.0f;
-        ears_texturePaint_group[0].brushTexture = brush_texture;
-        head_texturePaint_group[0].brushSize = brush_size * 0.8f;
-        head_texturePaint_group[0].brushTexture = brush_texture;
-        body_texturePaint_group[0].brushSize = brush_size * 0.6f;
-        body_texturePaint_group[0].brushTexture = brush_texture;
-        body1_texturePaint_group[0].brushSize = brush_size * 3.0f;
-        body1_texturePaint_group[0].brushTexture = brush_texture;
-        tail_texturePaint_group[0].brushSize = brush_size * 2.0f;
-        tail_texturePaint_group[0].brushTexture = brush_texture;
+        ears_texturePaint_group[0].brushSize_L = brush_size * 3.0f;
+        ears_texturePaint_group[0].brushTexture_L = brush_texture;
+        head_texturePaint_group[0].brushSize_L = brush_size * 0.8f;
+        head_texturePaint_group[0].brushTexture_L = brush_texture;
+        body_texturePaint_group[0].brushSize_L = brush_size * 0.6f;
+        body_texturePaint_group[0].brushTexture_L = brush_texture;
+        body1_texturePaint_group[0].brushSize_L = brush_size * 3.0f;
+        body1_texturePaint_group[0].brushTexture_L = brush_texture;
+        tail_texturePaint_group[0].brushSize_L = brush_size * 2.0f;
+        tail_texturePaint_group[0].brushTexture_L = brush_texture;
 
 /*        for (int i = 0; i < 4; i++)
         {
